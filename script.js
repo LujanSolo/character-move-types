277480e0da093c4e5c6af9dee13aef71584a9f20
LujanSolo <lujansolo@proton.me>
2023-01-10 00:30:25 -0800

    changed Enemy class x,y origin coordinates to math.random()

--- script.js
+++ script.js
@@ -3,31 +3,33 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-
+const numberOfNpcs = 100;
+const npcArray = [];
 
 class Enemy {
-  constructor(){
-    this.x = 10;
-    this.y = 50;
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
     this.width = 100;
     this.height = 100;
   }
-  update(){
+  update() {
     this.x++;
     this.y++;
   }
-  draw(){
+  draw() {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 };
 
-const enemy1 = new Enemy(); 
+// const enemy1 = new Enemy();
+for (let i = 0; i < numberOfNpcs; i++)
 
-function animate() {
-  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  enemy1.update();
-  enemy1.draw(); 
-  requestAnimationFrame(animate);
-};
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy1.update();
+    enemy1.draw();
+    requestAnimationFrame(animate);
+  };
 
 animate();
